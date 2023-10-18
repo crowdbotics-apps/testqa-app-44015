5c34dcc7474e87ef2b1efd6723e151013644fbc6
Mithilesh Tiwari <Mithilesh@crowdbotics.com>
2023-10-19 00:32:34 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- store/testqaappAPI/api.js
+++ store/testqaappAPI/api.js
@@ -1,52 +1,72 @@
-import axios from "axios"
+import axios from "axios";
 const testqaappAPI = axios.create({
   baseURL: "https://testqa-app-44015.botics.co",
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+  }
+});
+
 function api_docs_schema_retrieve(payload) {
   return testqaappAPI.get(`/api-docs/schema/`, {
-    params: { lang: payload.lang }
-  })
+    params: {
+      lang: payload.lang
+    }
+  });
 }
+
 function api_v1_login_create(payload) {
-  return testqaappAPI.post(`/api/v1/login/`, payload)
+  return testqaappAPI.post(`/api/v1/login/`, payload);
 }
+
 function api_v1_signup_create(payload) {
-  return testqaappAPI.post(`/api/v1/signup/`, payload)
+  return testqaappAPI.post(`/api/v1/signup/`, payload);
 }
+
 function rest_auth_login_create(payload) {
-  return testqaappAPI.post(`/rest-auth/login/`, payload)
+  return testqaappAPI.post(`/rest-auth/login/`, payload);
 }
+
 function rest_auth_logout_create(payload) {
-  return testqaappAPI.post(`/rest-auth/logout/`)
+  return testqaappAPI.post(`/rest-auth/logout/`);
 }
+
 function rest_auth_password_change_create(payload) {
-  return testqaappAPI.post(`/rest-auth/password/change/`, payload)
+  return testqaappAPI.post(`/rest-auth/password/change/`, payload);
 }
+
 function rest_auth_password_reset_create(payload) {
-  return testqaappAPI.post(`/rest-auth/password/reset/`, payload)
+  return testqaappAPI.post(`/rest-auth/password/reset/`, payload);
 }
+
 function rest_auth_password_reset_confirm_create(payload) {
-  return testqaappAPI.post(`/rest-auth/password/reset/confirm/`, payload)
+  return testqaappAPI.post(`/rest-auth/password/reset/confirm/`, payload);
 }
+
 function rest_auth_registration_create(payload) {
-  return testqaappAPI.post(`/rest-auth/registration/`, payload)
+  return testqaappAPI.post(`/rest-auth/registration/`, payload);
 }
+
 function rest_auth_registration_resend_email_create(payload) {
-  return testqaappAPI.post(`/rest-auth/registration/resend-email/`, payload)
+  return testqaappAPI.post(`/rest-auth/registration/resend-email/`, payload);
 }
+
 function rest_auth_registration_verify_email_create(payload) {
-  return testqaappAPI.post(`/rest-auth/registration/verify-email/`, payload)
+  return testqaappAPI.post(`/rest-auth/registration/verify-email/`, payload);
 }
+
 function rest_auth_user_retrieve(payload) {
-  return testqaappAPI.get(`/rest-auth/user/`)
+  return testqaappAPI.get(`/rest-auth/user/`);
 }
+
 function rest_auth_user_update(payload) {
-  return testqaappAPI.put(`/rest-auth/user/`, payload)
+  return testqaappAPI.put(`/rest-auth/user/`, payload);
 }
+
 function rest_auth_user_partial_update(payload) {
-  return testqaappAPI.patch(`/rest-auth/user/`, payload)
+  return testqaappAPI.patch(`/rest-auth/user/`, payload);
 }
+
 export const apiService = {
   api_docs_schema_retrieve,
   api_v1_login_create,
@@ -62,4 +82,4 @@ export const apiService = {
   rest_auth_user_retrieve,
   rest_auth_user_update,
   rest_auth_user_partial_update
-}
+};
